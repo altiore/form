31cadd7c15f6433f883888c01b1ce6e22de1d6f1
Razzwan <razvanlomov@gmail.com>
2021-09-22 12:22:18 +0300

    field(types): improved types and fix logical errors

--- src/form/form.stories.tsx
+++ src/form/form.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
-import {Field} from '../field/field';
 import {Form} from './form';
 
 export default {
@@ -12,30 +11,11 @@ export default {
 
 const Template: ComponentStory<typeof Form> = (args) => <Form {...args} />;
 
-export interface InputProps {
-	name: string;
-	meta: any;
-}
-
 export const SimplestForm = Template.bind({});
-
-const validate = (value: any) => {
-	return undefined;
-}
-
-const Input:React.FC<InputProps> = ({name, meta}) => {
-	console.log(meta)
-	return (
-		<input name={name} />
-	)
-}
-
 SimplestForm.args = {
 	children: (
 		<>
-			<Field name="test1" />
-			<Field name="test2" component={Input} />
-			<Field name="test3" component={Input} validate={validate}/>
+			<input name="test" />
 		</>
 	),
 };
