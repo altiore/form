34f421c095e5199d46aed2eb67311a2db3d4bb42
QraisinQ <hs1985irin@gmail.com>
2021-11-03 10:56:09 +0200

    feat(sample):added doc block with code example

--- src/form/form.stories.tsx
+++ src/form/form.stories.tsx
@@ -19,12 +19,84 @@ export const SimplestForm = Template.bind({});
 SimplestForm.args = {
 	children: (
 		<>
-			<input name="test" />
-			<Field name="field" label="Custom Field" />
+			<input name="name" />
 			<button type="submit">Submit</button>
 		</>
 	),
 	defaultValues: {
 		field: 'initial field',
+	},
+};
+SimplestForm.parameters = {
+	docs: {
+		source: {
+			code: `
+import React, {useCallback} from 'react';
+
+import {Form} from '@altiore/form';
+
+const MyForm = () => {
+	const handleSubmit = useCallback((values) => {
+		console.log('form.values is', values);
+	}, []);
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<input name="name" />
+			<button type="submit">Submit</button>
+		</Form>
+	);
+}
+`,
+		},
+	},
+};
+
+export const FormWithCustomField = Template.bind({});
+FormWithCustomField.args = {
+	children: (
+		<>
+			<Field label="Field Label" name="name" />
+			<button type="submit">Submit</button>
+		</>
+	),
+	defaultValues: {
+		field: 'initial field',
+	},
+};
+FormWithCustomField.parameters = {
+	docs: {
+		source: {
+			code: `
+import React, {useCallback} from 'react';
+
+import {createField, Form} from '@altiore/form';
+
+export const Field = createField(({errors, inputRef, name, /* you can add any extra fields here: */ label}) => {
+	return (
+		<div>
+			<label htmlFor="input-id">
+				{label}
+				<input id="input-id" name={name} ref={inputRef} />
+			</label>
+			<span>{errors[0]}</span>
+		</div>
+	);
+});
+
+const MyForm = () => {
+	const handleSubmit = useCallback((values) => {
+		console.log('form.values is', values);
+	}, []);
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Field label="Field Label" name="name" />
+			<button type="submit">Submit</button>
+		</Form>
+	);
+}
+`,
+		},
 	},
 };
