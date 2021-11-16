851e6700ad42771d75c0c83852c89ee3ee09a5ca
Razzwan <razvanlomov@gmail.com>
2021-11-16 14:40:55 +0200

    feat(form): FormProps наследует все свойства нативного html элемента form

--- src/form/types/form-props.ts
+++ src/form/types/form-props.ts
@@ -1,6 +1,7 @@
-import {ReactNode} from 'react';
+import {FormHTMLAttributes, ReactNode} from 'react';
 
-export interface FormProps<Values extends Record<string, any>> {
+export interface FormProps<Values extends Record<string, any>>
+	extends Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> {
 	children: ReactNode;
 	defaultValues?: Partial<Values>;
 	onSubmit: (values: Partial<Values>) => void;
