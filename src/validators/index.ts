135d6ef423891978ee998abfe7292eb605eb908a
bulakh <bulakh94@gmail.com>
2022-02-24 10:53:30 +0300

    feat(validators): добавляет isRequired

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -2,4 +2,5 @@ export * from './max-length';
 export * from './min-length';
 export * from './compare-password';
 export * from './is-email';
+export * from './is-required';
 export * from './regexp';
